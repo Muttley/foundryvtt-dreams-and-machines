778d7a3e19f29559999471143d333027eb743e0c
Paul Maskelyne <mu77ley@gmail.com>
2025-07-14 01:00:06 +0100

    delete old sheet

--- system/src/sheets/items/_module.mjs
+++ system/src/sheets/items/_module.mjs
@@ -1,6 +1,5 @@
 export {default as ArchetypeSheet} from "./ArchetypeSheetV2.mjs";
 export {default as ArmorSheet} from "./ArmorSheetV2.mjs";
-export {default as DnMItemSheet} from "../DnMItemSheet.mjs";
 export {default as EquipmentSheet} from "./EquipmentSheetV2.mjs";
 export {default as MajorNPCActionSheet} from "./MajorNPCActionSheetV2.mjs";
 export {default as NPCActionSheet} from "./NPCActionSheetV2.mjs";
